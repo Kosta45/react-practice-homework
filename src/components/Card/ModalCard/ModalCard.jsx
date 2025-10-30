c883e834ec591c7fe51bb77a8ecb300cec55734d
Kostiantyn <konstanlebed8@gmail.com>
2025-10-30 20:12:00 +0100

    Add ModalCard component with styling for product name

---
+++ src/components/Card/ModalCard/ModalCard.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+import Modal from "../../Modal/Modal";
+import ModalClose from "../../Modal/ModalClose/ModalClose";
+import ModalHeader from "../../Modal/Modalheader/ModalHeader";
+import ModalBody from "../../Modal/ModalBody/ModalBody";
+import ModalWrapper from "../../Modal/ModalWrapper/ModalWrapper";
+import ModalFooter from "../../Modal/ModalFooter/ModalFooter";
+
+import "./ModalCard.scss";
+
+function ModalCard(props) {
+  const { onClose, handleFirstClick, handleSecondClick, productName } = props;
+
+  return (
+    <ModalWrapper onClose={onClose}>
+      <Modal>
+        <ModalHeader>
+          <ModalClose onClick={onClose} />
+        </ModalHeader>
+
+        <ModalBody>
+          <h2>
+            Do you want to add{" "}
+            <span className="product-name">{productName}</span> to basket?
+          </h2>
+        </ModalBody>
+
+        <ModalFooter
+          firstClick={handleFirstClick}
+          firstText="Yes, Add to basket."
+          secondaryClick={handleSecondClick}
+          secondaryText="No, cancel"
+        ></ModalFooter>
+      </Modal>
+    </ModalWrapper>
+  );
+}
+
+export default ModalCard;
